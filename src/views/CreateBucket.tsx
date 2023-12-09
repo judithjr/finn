383892aa76e9c838a472199124873c63b0fb3159
Neel <neelp0980@gmail.com>
2023-12-09 14:00:01 +0530

    chore: added chips

--- src/views/CreateBucket.tsx
+++ src/views/CreateBucket.tsx
@@ -1,4 +1,4 @@
-import { useState, Fragment } from "react";
+import { useState, Fragment, Key } from "react";
 import { useStore } from "@/store";
 import Input from "@/components/form-elements/input";
 import { Listbox, Transition } from "@headlessui/react";
@@ -12,6 +12,7 @@ import {
   useWaitForTransaction,
 } from "wagmi";
 import { mumbaiAddress } from "@/utils/constants";
+import Image from "next/image";
 
 const networkOptions = ["Arbitrum", "Base"];
 const tokenOptions = [
@@ -19,54 +20,64 @@ const tokenOptions = [
     {
       name: "ARB",
       value: "0x912ce59144191c1204e64559fe8253a0e49e6548",
-      logoURI: "https://tokens.1inch.io/0xb50721bcf8d664c30412cfbc6cf7a15145234ad1.png"
+      logoURI:
+        "https://tokens.1inch.io/0xb50721bcf8d664c30412cfbc6cf7a15145234ad1.png",
     },
     {
       name: "WETH",
       value: "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
-      logoURI: "https://tokens.1inch.io/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png"
+      logoURI:
+        "https://tokens.1inch.io/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png",
     },
     {
       name: "WBTC",
       value: "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f",
-      logoURI: "https://tokens.1inch.io/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599.png"
+      logoURI:
+        "https://tokens.1inch.io/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599.png",
     },
     {
       name: "DAI",
       value: "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
-      logoURI: "https://tokens.1inch.io/0x6b175474e89094c44da98b954eedeac495271d0f.png"
+      logoURI:
+        "https://tokens.1inch.io/0x6b175474e89094c44da98b954eedeac495271d0f.png",
     },
     {
       name: "LINK",
       value: "0xf97f4df75117a78c1a5a0dbb814af92458539fb4",
-      logoURI: "https://tokens.1inch.io/0x514910771af9ca656af840dff83e8264ecf986ca.png"
+      logoURI:
+        "https://tokens.1inch.io/0x514910771af9ca656af840dff83e8264ecf986ca.png",
     },
     {
       name: "ETH",
       value: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-      logoURI: "https://tokens.1inch.io/0x2170ed0880ac9a755fd29b2688956bd959f933f8.png" 
+      logoURI:
+        "https://tokens.1inch.io/0x2170ed0880ac9a755fd29b2688956bd959f933f8.png",
     },
   ],
   [
     {
       name: "ETH",
       value: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-      logoURI: "https://tokens.1inch.io/0x2170ed0880ac9a755fd29b2688956bd959f933f8.png"
+      logoURI:
+        "https://tokens.1inch.io/0x2170ed0880ac9a755fd29b2688956bd959f933f8.png",
     },
     {
       name: "WETH",
       value: "0x4200000000000000000000000000000000000006",
-      logoURI: "https://tokens.1inch.io/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png"
+      logoURI:
+        "https://tokens.1inch.io/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png",
     },
     {
       name: "BASE",
       value: "0x0bf0ba3962a189d56f358143f38b7ffd26b8d48f",
-      logoURI: "https://tokens.1inch.io/0x07150e919b4de5fd6a63de1f9384828396f25fdc.png"
+      logoURI:
+        "https://tokens.1inch.io/0x07150e919b4de5fd6a63de1f9384828396f25fdc.png",
     },
     {
       name: "DAI",
       value: "0x50c5725949a6f0c72e6c4a641f24049a917db0cb",
-      logoURI: "https://tokens.1inch.io/0x6b175474e89094c44da98b954eedeac495271d0f.png"
+      logoURI:
+        "https://tokens.1inch.io/0x6b175474e89094c44da98b954eedeac495271d0f.png",
     },
   ],
 ];
@@ -130,12 +141,7 @@ export default function CreateBucket() {
             setBucketDesc(e.target.value);
           }}
         />
-        <Listbox
-          value={network}
-          onChange={() => {
-            setNetwork;
-          }}
-        >
+        <Listbox value={network} onChange={setNetwork}>
           <label className="text-teal-100 font-['Roobert'] text-md">
             Select Network
           </label>
@@ -247,6 +253,30 @@ export default function CreateBucket() {
             </div>
           </Listbox>
         ) : null} */}
+        <div>
+          {tokenOptions[0].map((token) => {
+            return (
+              <div className="flex w-32" key={token.name}>
+                <span className="inline-flex items-center px-2 gap-1 text-sm text-gray-200 bg-neutral-700 border rounded-e-0 border-teal-400 rounded-s-md">
+                  <Image
+                    src={token.logoURI}
+                    className="border border-teal-400 rounded-full"
+                    alt="token"
+                    width={20}
+                    height={20}
+                  />
+                  {token.name}
+                </span>
+                <input
+                  type="text"
+                  id="allocation"
+                  className="rounded-none rounded-e-lg bg-neutral-700 border text-gray-200 focus:ring-teal-400 focus:border-teal-400 block flex-1 min-w-0 w-full text-sm border-teal-400 p-2.5"
+                  placeholder="10%"
+                />
+              </div>
+            );
+          })}
+        </div>
 
         <div className="flex mt-2 items-center justify-center">
           <button
