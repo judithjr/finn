b3e68cb3382cf3d9c7c20afebf841184d0e7581f
vrajdesai78 <vrajdesai78@gmail.com>
2023-12-09 21:17:47 +0530

    Added wc-v3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,19 @@
-import Navbar from "@/components/NavBar";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { WagmiConfig } from "wagmi";
-import { Web3Modal } from "@web3modal/react";
-import { config, ethereumClient } from "@/utils/wagmiConfig";
+import Navbar from '@/components/NavBar';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { WagmiConfig } from 'wagmi';
+import { config, ethereumClient } from '@/utils/wagmiConfig';
+import { Web3Modal } from '@/context/web3Modal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <WagmiConfig config={config}>
+      <Web3Modal>
         <Navbar />
         <div className="min-h-[calc(100vh-68px)] pt-16 px-2 sm:px-4">
           <Component {...pageProps} />
         </div>
-      </WagmiConfig>
-      <Web3Modal
-        projectId={"33e28c5d43009b3668cccf62984e6dbe"}
-        ethereumClient={ethereumClient}
-      />
+      </Web3Modal>
     </>
   );
 }
