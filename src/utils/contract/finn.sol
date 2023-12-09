ea071109f3d333323f0ef465756125687bc83f3e
vrajdesai78 <vrajdesai78@gmail.com>
2023-12-10 03:28:42 +0530

    updated contract and abi

--- src/utils/contract/finn.sol
+++ src/utils/contract/finn.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
@@ -9,9 +9,7 @@ contract Bucket is ReentrancyGuard {
     using Counters for Counters.Counter;
 
     address public creator;
-    uint256 public totalValue;
     bool public isPublic;
-    uint256 public unlockTimestamp;
     mapping(address => uint256) public tokenProportions;
     mapping(address => uint256) public tokenBalances;
     string public name;
@@ -28,9 +26,7 @@ contract Bucket is ReentrancyGuard {
 
     event BucketCreated(
         address indexed creator,
-        uint256 totalValue,
         bool isPublic,
-        uint256 unlockTimestamp,
         address[] tokens,
         uint256[] proportions,
         string[] chains,
@@ -44,28 +40,22 @@ contract Bucket is ReentrancyGuard {
         _;
     }
 
-    modifier isUnlocked() {
-        require(block.timestamp >= unlockTimestamp, "Bucket is still locked");
-        _;
-    }
-
     constructor(
         address _creator,
-        uint256 _totalValue,
         bool _isPublic,
-        uint256 _lockDurationInDays,
         address[] memory _tokens,
         uint256[] memory _proportions,
         string[] memory _chains,
         string memory _name,
         string memory _description
     ) {
-        require(_tokens.length == _proportions.length && _chains.length > 0, "Invalid input");
+        require(
+            _tokens.length == _proportions.length && _chains.length > 0,
+            "Invalid input"
+        );
 
         creator = _creator;
-        totalValue = _totalValue;
         isPublic = _isPublic;
-        unlockTimestamp = block.timestamp + (_lockDurationInDays * 1 days);
         name = _name;
         description = _description;
         chains = _chains;
@@ -76,19 +66,19 @@ contract Bucket is ReentrancyGuard {
             require(_proportions[i] > 0, "Proportion must be greater than 0");
             totalProportion += _proportions[i];
             tokenProportions[_tokens[i]] = _proportions[i];
-            bucketsDetails.push(BucketStruct({
-                tokenAddress: _tokens[i],
-                share: _proportions[i],
-                chain: _chains[i]
-            }));
+            bucketsDetails.push(
+                BucketStruct({
+                    tokenAddress: _tokens[i],
+                    share: _proportions[i],
+                    chain: _chains[i]
+                })
+            );
         }
         require(totalProportion == 100, "Total proportion must be 100");
 
         emit BucketCreated(
             _creator,
-            _totalValue,
             _isPublic,
-            unlockTimestamp,
             _tokens,
             _proportions,
             _chains,
@@ -97,38 +87,43 @@ contract Bucket is ReentrancyGuard {
         );
     }
 
-    function addToken(address token, uint256 amount) external onlyCreator nonReentrant {
+    function addToken(
+        address token,
+        uint256 amount
+    ) external payable onlyCreator nonReentrant {
         require(amount > 0, "Amount must be greater than 0");
 
+        // Ensure the sent Ether value is equal to the specified token amount
         require(
-            IERC20(token).transferFrom(msg.sender, address(this), amount),
-            "Token transfer failed"
+            msg.value == 0,
+            "Ether value sent with the transaction should be 0"
         );
+
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
 
         tokenBalances[token] += amount;
 
         emit TokensLocked(token, amount);
     }
 
-    function withdrawTokens(address token) external onlyCreator isUnlocked nonReentrant {
+    function withdrawTokens(address token) external onlyCreator nonReentrant {
         require(tokenBalances[token] > 0, "No tokens to withdraw");
 
-        require(
-            IERC20(token).transfer(creator, tokenBalances[token]),
-            "Token transfer failed"
-        );
+        IERC20(token).approve(creator, tokenBalances[token]);
+
+        IERC20(token).transfer(creator, tokenBalances[token]);
 
         tokenBalances[token] = 0;
     }
 
-    function getBucketDetails(address bucketAddress)
+    function getBucketDetails(
+        address bucketAddress
+    )
         external
         view
         returns (
             address _creator,
-            uint256 _totalValue,
             bool _isPublic,
-            uint256 _unlockTimestamp,
             address[] memory _tokens,
             uint256[] memory _proportions,
             string[] memory _chains,
@@ -138,9 +133,7 @@ contract Bucket is ReentrancyGuard {
     {
         Bucket bucket = Bucket(bucketAddress);
         _creator = bucket.creator();
-        _totalValue = bucket.totalValue();
         _isPublic = bucket.isPublic();
-        _unlockTimestamp = bucket.unlockTimestamp();
         _name = bucket.name();
         _description = bucket.description();
 
@@ -169,21 +162,21 @@ contract BucketFactory {
     event BucketCreated(
         address indexed creator,
         address indexed bucket,
-        uint256 totalValue,
         bool isPublic,
-        uint256 unlockTimestamp,
         address[] tokens,
         uint256[] proportions,
         string[] chains,
         string name,
         string description
     );
-    event InvestmentMade(address indexed investor, address indexed bucket, uint256 amount);
+    event InvestmentMade(
+        address indexed investor,
+        address indexed bucket,
+        uint256 amount
+    );
 
     function createBucket(
-        uint256 _totalValue,
         bool _isPublic,
-        uint256 _lockDurationInDays,
         address[] memory _tokens,
         uint256[] memory _proportions,
         string[] memory _chains,
@@ -192,9 +185,7 @@ contract BucketFactory {
     ) external {
         Bucket newBucket = new Bucket(
             msg.sender,
-            _totalValue,
             _isPublic,
-            _lockDurationInDays,
             _tokens,
             _proportions,
             _chains,
@@ -207,32 +198,13 @@ contract BucketFactory {
         emit BucketCreated(
             msg.sender,
             address(newBucket),
-            _totalValue,
             _isPublic,
-            newBucket.unlockTimestamp(),
             _tokens,
             _proportions,
             _chains,
             _name,
             _description
         );
-    }
-
-    function investInBucket(address bucketAddress, uint256 amount) external payable {
-        require(amount > 0, "Amount must be greater than 0");
-
-        Bucket bucket = Bucket(bucketAddress);
-        require(bucket.isPublic() || bucket.creator() == msg.sender, "Bucket not public or not created by the investor");
-
-        require(
-            IERC20(bucketAddress).transferFrom(msg.sender, address(bucket), amount),
-            "Token transfer failed"
-        );
-
-        bucket.addToken(bucketAddress, amount);
-        userInvestedBuckets[msg.sender].push(bucketAddress);
-
-        emit InvestmentMade(msg.sender, bucketAddress, amount);
     }
 
     function getNumBuckets() external view returns (uint256) {
@@ -243,11 +215,15 @@ contract BucketFactory {
         return allBuckets;
     }
 
-    function getBucketsByCreator(address creator) external view returns (address[] memory) {
+    function getBucketsByCreator(
+        address creator
+    ) external view returns (address[] memory) {
         return userToBuckets[creator];
     }
 
-    function getInvestedBuckets(address investor) external view returns (address[] memory) {
+    function getInvestedBuckets(
+        address investor
+    ) external view returns (address[] memory) {
         return userInvestedBuckets[investor];
     }
 }
